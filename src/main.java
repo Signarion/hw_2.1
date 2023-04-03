b8366da1e83b3f91940c57ca36014566955596f7
Signariona <signarion@yandex.ru>
2023-04-03 20:03:55 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+        int price = 1000;
+        int oneMile = 20;
+        int mile = price / oneMile;
+
+        System.out.println("Начислится " + mile + " миль");
+    }
+}
